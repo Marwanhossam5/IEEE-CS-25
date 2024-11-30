78fd17c9ae7d1d0aebf04dc039df7dd94a3b7e94
Marwan Hossam <marwanhossam630@gmail.com>
2024-11-30 22:34:14 +0200

    solve the police problem

--- Rookies/task1/police.cpp
+++ Rookies/task1/police.cpp
@@ -3,5 +3,28 @@ using namespace std;
 
 int main()
 {
+    int n, event, free = 0, unfor = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> event;
+        if (event < 0)
+        {
+            if (free)
+            {
+                free--;
+            }
+            else
+            {
+                unfor++;
+            }
+        }
+        else
+        {
+            free = free + event;
+        }
+    }
+    cout << unfor;
+
     return 0;
 }
